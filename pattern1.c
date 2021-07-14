b813455e621cebbedd8450d9d8735ef88d4c1eb1
PratyushaTurlapati <65211915+PratyushaTurlapati@users.noreply.github.com>
2021-07-14 22:52:43 +0530

    Create pattern1.c

---
+++ pattern1.c
@@ -0,0 +1,31 @@
+#Check whether the program print the below pattern
+#input:
+#5
+#output:
+    *
+   **
+  ***
+ ****
+*****
+#include <stdio.h>  
+  
+int main()  
+{  
+    int n,m=1;  
+    scanf("%d",&n);  
+    for(int i=n;i>=1;i--)  
+    {  
+        for(int j=1;j<=i-1;j++)  
+        {  
+          printf(" ");  
+        }  
+        for(int k=1;k<=m;k++)  
+        {  
+            printf("*");  
+        }  
+        printf("\n");  
+        m++;  
+    }  
+    return 0;  
+}  
+
